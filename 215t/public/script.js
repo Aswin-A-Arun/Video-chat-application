3ea7980d505f264c2aaff82c364ee7d6a0c39764
Enygma <57713995+Aswin-A-Arun@users.noreply.github.com>
2022-06-15 19:41:07 +0530

    Update script.js

--- 215t/public/script.js
+++ 215t/public/script.js
@@ -107,22 +107,22 @@ $(function () {
         }
     })
 
-    $("invite_button").click(function(){
-        const to = prompt('enter the email adress')
+    $("#invite_button").click(function () {
+        const to = prompt("Enter the email address")
         let data = {
             url: window.location.href,
-            to:to
+            to: to
         }
         $.ajax({
-            url:'/send-mail',
-            type:'post',
-            data:JSON.stringify(data),
-            dataType:'json',
-            contentType:'application/json',
-            success:function(result){
-                alert('invite sent')
+            url: "/send-mail",
+            type: "post",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (result) {
+                alert("Invite sent!")
             },
-            error:function(result){
+            error: function (result) {
                 console.log(result.responseJSON)
             }
         })
